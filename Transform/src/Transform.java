ebcd9976d9be6cd841915e8f2e7779d7ee876c2f
ChenKuo <ckuo1@umbc.edu>
2016-08-31 14:22:04 -0400

    closest node

--- Transform/src/Transform.java
+++ Transform/src/Transform.java
@@ -1,4 +1,8 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 
+import com.google.gson.Gson;
 
 public class Transform{
 	
@@ -6,7 +10,7 @@ public class Transform{
 	 *  A bare-bones collection of static methods for manipulating
 	 *  matrices.
 	 *http://introcs.cs.princeton.edu/java/22library/Matrix.java.html
-	 ******************************************************************************/
+	 ******************************************************************************
 
    // return n-by-n identity matrix I
     private static double[][] identity(int n) {
@@ -43,7 +47,7 @@ public class Transform{
         return y;
     }
 
-	/**********End of Matrix Utility Mathods*************************************/
+	**********End of Matrix Utility Mathods*************************************/
     
     
 	private final static int DisplayResolutionX = 1280;
@@ -73,7 +77,7 @@ public class Transform{
 			
 		}
 		
-		double[][] reverse= identity(3);
+		/**double[][] reverse= identity(3);*/
 		String transform=transforms[graph];
 		
 		String [] splitTransforms=transform.split("\\)");
@@ -85,26 +89,123 @@ public class Transform{
 			String[] nameVal=tstring.split("\\(");
 			if(nameVal[0].equals("translate")){
 				String[] xy=nameVal[1].split(",");
-				reverse=Transform.multiply(new double[][]{{1,0,-Double.parseDouble(xy[0])},{0,1,-Double.parseDouble(xy[1])},{0,0,1}},reverse);
+				/**reverse=Transform.multiply(new double[][]{{1,0,-Double.parseDouble(xy[0])},{0,1,-Double.parseDouble(xy[1])},{0,0,1}},reverse);*/
+				x-=Double.parseDouble(xy[0]);
+				y-=Double.parseDouble(xy[1]);
 			}
 			else if(nameVal[0].equals("scale")){
 				String[] xy=nameVal[1].split(",");
-				reverse=Transform.multiply(new double[][]{{1/Double.parseDouble(xy[0]),0,0},{0,1/Double.parseDouble(xy[xy.length-1]),0},{0,0,1}},reverse);
+				/**reverse=Transform.multiply(new double[][]{{1/Double.parseDouble(xy[0]),0,0},{0,1/Double.parseDouble(xy[xy.length-1]),0},{0,0,1}},reverse);*/
+				x/=Double.parseDouble(xy[0]);
+				y/=Double.parseDouble(xy[xy.length-1]);
 			}else throw new RuntimeException("unusual transform string: "+transform);
 		}
 		
-		double[] graphPoint=Transform.multiply(reverse,new double[]{x,y,1});
+		/**double[] graphPoint=Transform.multiply(reverse,new double[]{x,y,1});
 		result[0]=graphPoint[0];
-		result[1]=graphPoint[1];
+		result[1]=graphPoint[1];*/
+		result[0]=x;
+		result[1]=y;
+		
 		return graph;	
 	}
 	
-	//test client
+	public static boolean isGraphPointVisible(double graphX, double graphY, String transform, double left, double right, double height){
+
+		String [] splitTransforms=transform.split("\\)");
+		if(transform.indexOf("graph_hidden")==0){
+			return false;
+		}	
+		for(int i=splitTransforms.length-1;i>=0;i--){
+			String tstring=splitTransforms[i];
+			if(tstring.equals("\n")) continue;
+			String[] nameVal=tstring.split("\\(");
+			if(nameVal[0].equals("translate")){
+				String[] xy=nameVal[1].split(",");
+				graphX+=Double.parseDouble(xy[0]);
+				graphY+=Double.parseDouble(xy[1]);
+			}
+			else if(nameVal[0].equals("scale")){
+				String[] xy=nameVal[1].split(",");
+				graphX*=Double.parseDouble(xy[0]);
+				graphY*=Double.parseDouble(xy[xy.length-1]);
+			}else throw new RuntimeException("unusual transform string: "+transform);
+		}
+
+		return graphX>left && graphX<right && graphY>0 && graphY<height;
+	}
+	
+	//test client, remove this after testing
 	public static void main(String[] args){
 		double[] result=new double[2];
-		String[] trans={"translate(830,497)scale(1, 1)scale(1,1)translate(-415, 0)translate(-98,-102)scale(1)","translate(830,497)scale(-1, 1)scale(1,1)translate(-415, 0)translate(-98,-102)scale(1)"};
-		int graph=Transform.GazePointToGraphPoint(656, 487, trans, 13, 68, 1660, 994, result);
-		System.out.println(graph);
-		System.out.println(result[0]+" "+result[1]);
+		String[] trans={"translate(415,497)scale(1,1)translate(0,0)scale(1)",	"translate(1245,497)scale(1,1)translate(0,0)scale(1)"};
+		double gazeX=656;
+		double gazeY=412;
+		int qid=0;
+		int graph=Transform.GazePointToGraphPoint(gazeX, gazeY, trans, 13, 68, 1660, 994, result);
+		System.out.println("graph: "+graph);
+		System.out.println("coordinate on graph: "+result[0]+" "+result[1]);
+		NodesLocationManager NLM=new NodesLocationManager();
+		Node closestNode=NLM.getClosestNode(qid,result[0],result[1]);
+		if(closestNode!=null){
+			double left= (graph==0)? 0 : 1660/2;
+			double right= (graph==1||trans.length==1)? 1660: 1660/2;
+			boolean nodeVisible=Transform.isGraphPointVisible(closestNode.x, closestNode.y, trans[graph], left, right,994);
+			System.out.println("closest node: "+closestNode.id+"\tvisible: "+nodeVisible);
+		}
+		else{
+			System.out.println("no node within 40px radius");
+		}
+		
 	}
+}
+
+class NodesLocationManager{
+	private String graphDataDirectory="E:\\XAMPP\\htdocs\\GraphStudy2\\php\\";//change this to your directory
+	private int currentQuestion=-1;
+	private Node[] nodes;
+	private int maxDistance=40;
+	
+	private void loadNodes(int qid){
+		Gson gson = new Gson();
+		Reader reader = null;
+		try{
+			reader=new FileReader(graphDataDirectory+"graph-data"+qid+"\\graph-nodes.txt");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}finally{
+				nodes=gson.fromJson(reader,Node[].class);
+			}
+	}
+	public Node getClosestNode(int questionID, double x, double y){
+		if(currentQuestion!=questionID){
+			loadNodes(questionID);
+			currentQuestion=questionID;
+		}
+		double distance=maxDistance;
+		Node closest = null;
+		for(Node n:nodes){
+			if(Math.abs(n.x-x)<distance&&Math.abs(n.y-y)<distance){
+				double d=distance(n.x,n.y,x,y);
+				if(d<distance){
+					distance=d;
+					closest=n;
+				}
+			}
+		}
+
+		return closest;
+
+	}
+	private static double distance(double x1, double y1, double x2, double y2){
+		return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2, 2));
+	}
+}
+
+class Node{
+	int id;
+	String location;
+	String type;
+	double x;
+	double y;
 }
